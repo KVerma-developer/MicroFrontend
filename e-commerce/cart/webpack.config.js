f8045ab286ca7f323da9a82cb20b11127b43736b
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-07-12 21:59:17 +0530

    federate cart and product with refactored(mount) both

--- e-commerce/cart/webpack.config.js
+++ e-commerce/cart/webpack.config.js
@@ -11,7 +11,7 @@ module.exports={
         new ModuleFederationPlugin({
             name:"cart",
             filename:"remoteEntry.js",
-            exposes:{ "./CartShow":"./src/index.js"},
+            exposes:{ "./CartShow":"./src/bootstrap.js"},
             // shared:['faker']
             shared:{
                 faker:{
--- e-commerce/container/src/bootstrap.js
+++ e-commerce/container/src/bootstrap.js
@@ -1,7 +1,9 @@
 // import 'productsApp/ProductsIndex' ///this productsApp in webpack.config.js
-import { mount } from '../../products/src/bootstrap'
-import 'cartApp/CartShow'
+import { mount as productMount} from '../../products/src/bootstrap'
+// import 'cartApp/CartShow'
+import { mount as cartMount} from '../../cart/src/bootstrap';
 //here products , cartApp files loaded up asynchronously in container
 
 console.log('container project')
-mount(document.querySelector('#my-products'));
+productMount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'))
